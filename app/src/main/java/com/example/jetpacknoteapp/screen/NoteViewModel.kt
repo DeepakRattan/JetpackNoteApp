e1bbeaef066b0c3e91e5c231772d696deb0b8fab
Deepak Rattan <deepak.rattan@itechnolabs.tech>
2023-04-13 20:36:16 +0530

    Updates NoteViewModel class for accessing data from NoteRepository

--- app/src/main/java/com/example/jetpacknoteapp/screen/NoteViewModel.kt
+++ app/src/main/java/com/example/jetpacknoteapp/screen/NoteViewModel.kt
@@ -1,34 +1,67 @@
 package com.example.jetpacknoteapp.screen
 
 import android.os.Build
+import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.jetpacknoteapp.data.NotesDataSource
 import com.example.jetpacknoteapp.model.Note
+import com.example.jetpacknoteapp.repository.NoteRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 /**
  * Created by Deepak Rattan on 12/04/23
  */
 
 @RequiresApi(Build.VERSION_CODES.O)
-class NoteViewModel : ViewModel() {
-    var noteList = mutableStateListOf<Note>()
+@HiltViewModel
+class NoteViewModel @Inject constructor(private val noteRepository: NoteRepository) : ViewModel() {
+    //    var noteList = mutableStateListOf<Note>()
+    private val _noteList = MutableStateFlow<List<Note>>(emptyList())
+    val noteList = _noteList.asStateFlow()
+
+    companion object {
+        const val TAG = "NoteViewModel"
+    }
 
     init {
-        noteList.addAll(NotesDataSource().loadNotes())
+        viewModelScope.launch(Dispatchers.IO) {
+            noteRepository.getAllNotes().distinctUntilChanged().collect { listOfNotes ->
+                if (listOfNotes.isNullOrEmpty()) {
+                    Log.d(TAG, ":Empty List ")
+
+                } else {
+                    _noteList.value = listOfNotes
+                }
+            }
+        }
     }
 
     fun addNote(note: Note) {
-        noteList.add(note)
+        viewModelScope.launch {
+            noteRepository.addNote(note)
+        }
     }
 
     fun removeNote(note: Note) {
-        noteList.remove(note)
+        viewModelScope.launch {
+            noteRepository.deleteNote(note)
+        }
     }
 
-    fun getAllNotes(): List<Note> {
-        return noteList
+    fun getAllNotes() {
+        viewModelScope.launch {
+            noteRepository.getAllNotes()
+        }
     }
 }
